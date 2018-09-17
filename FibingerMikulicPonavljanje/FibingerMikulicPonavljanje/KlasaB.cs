379aef3d58fbcaea1dc6383514fb61a26ca801c8
BrankoFibinger <35739422+BrankoFibinger@users.noreply.github.com>
2018-09-17 12:16:34 +0200

    Add files via upload

---
+++ FibingerMikulicPonavljanje/FibingerMikulicPonavljanje/KlasaB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibingerMikulicPonavljanje
+{
+    class KlasaB
+    {
+        int zbroj;
+        double prosjek;
+
+        public double GetProsjek()
+        {
+            return prosjek;
+        }
+
+        public void SetProsjek(double value)
+        {
+            prosjek = value;
+        }
+
+        public int GetZbroj()
+        {
+            return zbroj;
+        }
+
+        public void SetZbroj(int value)
+        {
+            zbroj = value;
+        }
+        double broj = 0;
+        public int Zbroj (int[]zbrojNiz)
+        {
+            
+            for (int i=0;i<5;i++)
+            {
+                broj = broj + zbrojNiz[i];
+            }
+            return Convert.ToInt32(broj);
+
+        }
+        double broj2;
+        public double Prosjek (int [] prosjekNiz)
+        {
+            for (int j=0;j<5;j++)
+            {
+                broj2 = broj / 5;
+            }
+            return broj2;
+        }
+    }
+}
---
+++ FibingerMikulicPonavljanje/FibingerMikulicPonavljanje/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibingerMikulicPonavljanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaB klasa = new KlasaB();
+            int [] niz = new int[5];
+            Console.WriteLine("Unesite 5 brojeva.");
+            for (int i=0;i<5;i++)
+            {
+                niz[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            int zbroj = klasa.Zbroj(niz);
+            double prosjek = klasa.Prosjek(niz);
+            Console.WriteLine("Zbroj:{0}",zbroj);
+            Console.WriteLine("\nProsjek:{0}",prosjek);
+            Console.ReadKey();
+        }
+    }
+}
